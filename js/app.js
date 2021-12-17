03e1afb2dbc39f22b7f223473886968c77508a38
Srikanth <nandiraju@yahoo.com>
2021-12-17 18:46:11 +0530

    Sivananda Guru

---
+++ js/app.js
@@ -0,0 +1,69 @@
+var $$ = Dom7;
+var app = new Framework7({
+    touch: {
+        fastClicks: true,
+    },
+    statusbar: {
+        enable: false
+    },
+    dialog: {
+        title: 'Moviestr'
+    },
+    view: {
+        stackPages: true
+    },
+    root: "#app", // App root element
+    id: "com.nandiraju.social", // App bundle ID
+    name: "Social Login", // App name
+    theme: "md", // Automatic theme detection
+    routes: [{
+        path: '/',
+        componentUrl: './pages/landing.html',
+        // componentUrl: './pages/home.html',
+        name: 'landing',
+        options: {
+            transition: 'f7-push',
+        }
+    }, {
+        path: '/star_form/',
+        componentUrl: './pages/star_form.html',
+        name: 'place-detail',
+        options: {
+            transition: 'f7-push',
+        }
+    }, {
+        path: '/pooja-types/',
+        componentUrl: './pages/pooja_types.html',
+        name: 'pooja-types',
+        options: {
+            transition: 'f7-push',
+        }
+    }]
+});
+var mainView = app.views.create(".view-main", {
+    url: "/"
+});
+
+Framework7.request.setup({
+    // headers: {
+    //     'Ocp-Apim-Subscription-Key': '6456ac8bbfb54cdf9a23208b64b1612a'
+    // }
+});
+
+function loadScreen(path) {
+    mainView.router.navigate(path);
+}
+
+function goBack() {
+    mainView.router.back();
+}
+
+function showToast(message, position) {
+    if (!position) position = "top";
+    var toastCenter = app.toast.create({
+        text: message,
+        position: "bottom",
+        closeTimeout: 2000
+    });
+    toastCenter.open();
+}
